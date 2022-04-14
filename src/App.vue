6525f4a936c3332283faaccb7e507aacc3e90397
Sidney Sherrill <sidneysherrill@Sidneys-MacBook-Pro.local>
2022-04-13 22:12:20 -0400

    Create list of pokemon w/ poke API (closes #2)

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,27 @@
-<script setup></script>
+<script setup>
+import { ref, onMounted } from "vue";
+
+// reactive state
+
+const pokemon = ref([]);
+
+// lifecycle hooks
+onMounted(() => {
+  fetch("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=10000")
+    .then((response) => response.json())
+    .then((data) => {
+      pokemon.value = data.results;
+    });
+});
+</script>
 
 <template>
   <h1>Pokédex</h1>
+  <ol>
+    <li class="capitalize" v-for="p in pokemon">
+      {{ p.name }}
+    </li>
+  </ol>
 </template>
 
 <style></style>
