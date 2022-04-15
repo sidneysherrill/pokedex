36b6aea0814f8215424f4fc24b99aebb320b6f08
Sidney Sherrill <sidneysherrill@Sidneys-MacBook-Pro.local>
2022-04-14 20:37:50 -0400

    Show detail of pokemon w/ pokeAPI (closes #8)

--- src/pages/Single.vue
+++ src/pages/Single.vue
@@ -1,4 +1,23 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import { useRoute } from "vue-router";
+
+// reactive state
+const route = useRoute();
+const pokemon = ref(null); //TO DO: don't need array, need single pokemon
+
+// lifecycle hooks
+onMounted(() => {
+  fetch(`https://pokeapi.co/api/v2/pokemon/${route.params.name}/`)
+    .then((response) => response.json())
+    .then((data) => {
+      pokemon.value = data; //TO DO: don't need results, need single pokemon
+    });
+});
+</script>
+
 <template>
   <p class="capitalize">{{ $route.params.name }}</p>
   <router-link to="/">Back</router-link>
+  <p v-if="pokemon">{{ pokemon }}</p>
 </template>
