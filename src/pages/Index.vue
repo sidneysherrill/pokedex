9d0b0e6240dc27654102b6847c744c737fa843c0
Sidney Sherrill <sidneysherrill@gmail.com>
2022-04-20 18:41:35 -0400

    Styled some components (color, titles, hover)

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -10,26 +10,30 @@ onMounted(() => {
 </script>
 
 <template>
-  <h1>Pokédex</h1>
-  <ol class="grid sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
-    <li
-      class="capitalize shadow hover:shadow-lg rounded-md p-1"
-      v-for="(p, i) in pokemon.list"
-    >
-      <router-link :to="`/pokemon/${p.name}`">
-        <img
-          class="w-full"
-          :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
-            i + 1
-          }.png`"
-          loading="lazy"
-          width="100"
-          height="100"
-        />
-        {{ p.name }}</router-link
-      >
-    </li>
-  </ol>
+  <div class="bg-slate-200">
+    <h1 class="text-4xl py-8 px-4 font-family">Pokédex</h1>
+    <div>
+      <ol class="grid sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
+        <li
+          class="capitalize font-mono hover:italic hover:font-bold shadow hover:shadow-lg rounded-md p-1 bg-amber-200 hover:bg-amber-300"
+          v-for="(p, i) in pokemon.list"
+        >
+          <router-link :to="`/pokemon/${i + 1}`">
+            <img
+              class="w-full"
+              :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${
+                i + 1
+              }.png`"
+              loading="lazy"
+              width="100"
+              height="100"
+            />
+            {{ p.name }}</router-link
+          >
+        </li>
+      </ol>
+    </div>
+  </div>
 </template>
 
 <style>
--- src/pages/Single.vue
+++ src/pages/Single.vue
@@ -17,11 +17,21 @@ onMounted(() => {
 </script>
 
 <template>
-  <router-link to="/">Back</router-link>
-  <p class="capitalize">{{ id }}</p>
-  <div class="capitalize" v-if="pokemon.active">
+  <div class="mx-6 mt-6">
+    <router-link
+      class="border-2 border-black rounded-lg px-3.5 py-1 bg-slate-300 hover:bg-slate-400 float-right"
+      to="/"
+      >Back</router-link
+    >
+  </div>
+  <p class="capitalize text-4xl my-6 mx-4 font-mono">{{ id }}</p>
+  <div
+    class="capitalize font-mono p-8 border-8 bg-lime-200"
+    v-if="pokemon.active"
+  >
     <pokemon-species :species="pokemon.active.species.name" />
     <img
+      class="float-right"
       :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.active.order}.png`"
     />
     <pokemon-info
@@ -29,6 +39,8 @@ onMounted(() => {
       :weight="pokemon.active.weight"
       :height="pokemon.active.height"
     />
-    <pokemon-stats :stats="pokemon.active.stats" />
+    <div>
+      <pokemon-stats :stats="pokemon.active.stats" />
+    </div>
   </div>
 </template>
