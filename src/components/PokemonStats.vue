ebab2f860432e969ff0b51652ed0e249c38d4fa4
Sidney Sherrill <sidneysherrill@gmail.com>
2022-04-20 19:35:13 -0400

    Created Stats chart with list elements

--- src/components/PokemonStats.vue
+++ src/components/PokemonStats.vue
@@ -5,10 +5,22 @@ const props = defineProps({
 </script>
 
 <template>
-  <h2>Stats</h2>
-  <ul>
-    <li v-for="s in props.stats">
-      {{ s.stat.name }}: {{ s.base_stat }} {{ s.effort }}
-    </li>
-  </ul>
+  <article class="bg-white p-6 rounded-lg">
+    <h2 class="font-bold text-xl">Stats</h2>
+    <ul class="grid grid-cols-6 gap-1">
+      <li v-for="s in props.stats">
+        <ul class="flex flex-col-reverse">
+          <li
+            v-for="i in 15"
+            :key="i"
+            class="h-2 bg-slate-100 mb-1"
+            :class="{ 'bg-emerald-300': s.base_stat >= i * 15 }"
+          ></li>
+        </ul>
+        <p class="text-center text-xs font-bold">
+          {{ s.stat.name.replace("-", " ") }}
+        </p>
+      </li>
+    </ul>
+  </article>
 </template>
