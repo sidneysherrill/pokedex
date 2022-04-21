74eff1f3bdc9fa8c2cd5da85cca3c18f65bba4b0
Sidney Sherrill <sidneysherrill@gmail.com>
2022-04-21 08:37:46 -0400

    Filled in About page

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@ import Navbar from "@/components/Navbar.vue";
 </script>
 
 <template>
-  <div class="max-w-6xl px-6 mx-auto">
+  <div class="max-w-6xl px-6 mx-auto mb-12">
     <navbar />
     <router-view></router-view>
   </div>
--- src/pages/About.vue
+++ src/pages/About.vue
@@ -13,60 +13,58 @@
       />
       <h2 class="font-bold text-xl mb-2">Sidney Sherrill</h2>
       <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga voluptate
-        nostrum reprehenderit soluta reiciendis eligendi quis iusto enim eos,
-        officia veritatis, accusantium, libero qui eum praesentium dignissimos
-        illo possimus perferendis?
+        I’m a junior web developer who creates intuitive user experiences and
+        prioritizes following best practices. I’ve enjoyed implementing this
+        Pokédex application using modern technologies such as Tailwind and
+        Pinia. My goal with this project was to make a performant and useable
+        single-page application that consumes an external API.
       </p>
     </article>
     <article class="shadow hover:shadow-lg rounded-md p-4 bg-white">
       <img src="@/assets/vue.svg" alt="" class="w-12 float-right" />
       <h2 class="font-bold text-xl mb-2">Vue</h2>
       <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga voluptate
-        nostrum reprehenderit soluta reiciendis eligendi quis iusto enim eos,
-        officia veritatis, accusantium, libero qui eum praesentium dignissimos
-        illo possimus perferendis?
+        A versatile framework for building web user interfaces. Vue.js laid the
+        groundwork for the application by allowing for smaller web components
+        rather than monolithic HTML, CSS, and JavaScript files.
       </p>
     </article>
     <article class="shadow hover:shadow-lg rounded-md p-4 bg-white">
       <img src="@/assets/pokeball.png" alt="" class="w-12 float-right" />
       <h2 class="font-bold text-xl mb-2">PokéAPI</h2>
       <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga voluptate
-        nostrum reprehenderit soluta reiciendis eligendi quis iusto enim eos,
-        officia veritatis, accusantium, libero qui eum praesentium dignissimos
-        illo possimus perferendis?
+        All Pokemon data was pulled from this RESTful API using the browser’s
+        built-in fetch module. I created helper methods to abstract interaction
+        with the API instead of making the fetch request directly within the
+        component.
       </p>
     </article>
     <article class="shadow hover:shadow-lg rounded-md p-4 bg-white">
       <img src="@/assets/router.png" alt="" class="w-12 float-right" />
       <h2 class="font-bold text-xl mb-2">Router</h2>
       <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga voluptate
-        nostrum reprehenderit soluta reiciendis eligendi quis iusto enim eos,
-        officia veritatis, accusantium, libero qui eum praesentium dignissimos
-        illo possimus perferendis?
+        The official router for Vue.js, which provides functionality to show
+        different components based on the URL in the address bar. This improves
+        the organization and user experience of the application.
       </p>
     </article>
     <article class="shadow hover:shadow-lg rounded-md p-4 bg-white">
       <img src="@/assets/pinia.svg" alt="" class="w-12 float-right px-1.5" />
       <h2 class="font-bold text-xl mb-2">Pinia</h2>
       <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga voluptate
-        nostrum reprehenderit soluta reiciendis eligendi quis iusto enim eos,
-        officia veritatis, accusantium, libero qui eum praesentium dignissimos
-        illo possimus perferendis?
+        A data store that manages the global state of the Pokédex. It allows us
+        to abstract the tougher application logic by simplifying components
+        using things like actions & getters. It serves to consolidate state
+        management, allowing pages to load quicker.
       </p>
     </article>
     <article class="shadow hover:shadow-lg rounded-md p-4 bg-white">
       <img src="@/assets/tailwind.svg" alt="" class="w-12 float-right" />
       <h2 class="font-bold text-xl mb-2">Tailwind CSS</h2>
       <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga voluptate
-        nostrum reprehenderit soluta reiciendis eligendi quis iusto enim eos,
-        officia veritatis, accusantium, libero qui eum praesentium dignissimos
-        illo possimus perferendis?
+        A utility-first CSS framework that offers a well-optimized library of
+        CSS classes. This allows for better consistency and performance when
+        styling the application because we don’t need to write our own CSS.
       </p>
     </article>
     <article class="shadow hover:shadow-lg rounded-md p-4 bg-white">
@@ -77,21 +75,15 @@
       />
       <h2 class="font-bold text-xl mb-2">Local Storage</h2>
       <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga voluptate
-        nostrum reprehenderit soluta reiciendis eligendi quis iusto enim eos,
-        officia veritatis, accusantium, libero qui eum praesentium dignissimos
-        illo possimus perferendis?
+        Persists data for users between browser visits. This is a technology
+        that enables the favorites feature. In the future, I’d like to use a
+        server-side database to persist the favorites.
       </p>
     </article>
     <article class="shadow hover:shadow-lg rounded-md p-4 bg-white">
       <img src="@/assets/github.svg" alt="" class="w-12 float-right px-1.5" />
       <h2 class="font-bold text-xl mb-2">GitHub</h2>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga voluptate
-        nostrum reprehenderit soluta reiciendis eligendi quis iusto enim eos,
-        officia veritatis, accusantium, libero qui eum praesentium dignissimos
-        illo possimus perferendis?
-      </p>
+      <p>I used GitHub to plan, store, version, build, and deploy my code.</p>
     </article>
   </div>
 </template>
