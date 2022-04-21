9c68272aa3392837b43a0421aa0756f537231b9b
Sidney Sherrill <sidneysherrill@gmail.com>
2022-04-21 08:18:24 -0400

    Improved empty state of favorites page

--- src/pages/Favorites.vue
+++ src/pages/Favorites.vue
@@ -12,9 +12,14 @@ onMounted(() => {
 
 <template>
   <div>
-    <h1 class="text-4xl pb-8 px-4">Favorites</h1>
+    <h1 class="text-3xl sm:text-4xl pb-8 px-4 text-center sm:text-left">
+      Favorites
+    </h1>
     <div>
-      <ol class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
+      <ol
+        v-if="pokemon.favoritesList && pokemon.favoritesList.length"
+        class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4"
+      >
         <li
           class="capitalize font-mono hover:italic hover:font-bold shadow hover:shadow-lg rounded-md p-1 bg-amber-200 hover:bg-amber-300"
           v-for="p in pokemon.favoritesList"
@@ -33,6 +38,20 @@ onMounted(() => {
           </router-link>
         </li>
       </ol>
+      <div v-else class="mt-48 flex justify-center items-center">
+        <div class="bg-white hover:shadow p-6 rounded max-w-lg">
+          <p class="text-lg text-center">
+            You have not favorited anything yet. Visit the the list page to
+            browse available Pokémon, then select your favorites.
+          </p>
+          <router-link
+            to="/"
+            class="block text-center rounded mt-4 px-4 py-2 bg-black text-white max-w-xs mx-auto"
+          >
+            Browse Pokémon
+          </router-link>
+        </div>
+      </div>
     </div>
   </div>
 </template>
